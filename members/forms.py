f7a080f2dcead745b9e21e1f483ae73dd20b7896
ch405maki <markmanuel0317@gmail.com>
2023-07-29 21:40:47 +0800

    Restart making form

--- members/forms.py
+++ members/forms.py
@@ -4,4 +4,4 @@ from .models import Member
 class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
-        fields = ['name', 'age', 'email', 'profile_picture']
+        fields = ['name', 'mname', 'lname', 'sname', 'birthday', 'age', 'bplace', 'email', 'profile_picture', ]
--- members/models.py
+++ members/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext as _
+import datetime
+
+def current_datetime():
+    return datetime.datetime.now()
 
 class Member(models.Model):
-    # Your existing fields
+
+    # Data fields
     name = models.CharField(max_length=100)
+    mname = models.CharField(max_length=100, default='')
+    lname = models.CharField(max_length=100, default='')
+    sname = models.CharField(max_length=100, default='', blank=True)
+    birthday = models.DateField(null=True, blank=True)
     age = models.IntegerField()
+    bplace = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField()
     profile_picture = models.ImageField(default='profile.png', upload_to='profile_pics')
 
